70ef579da922bfb643dec60752ed36c76abc56fb
Krofah <amponsahkrofah@gmail.com>
2023-10-18 17:20:44 +0000

    task 0

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,28 +9,36 @@ from functools import wraps
 store = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def count_url_access(func):
+    """ Decorator counting how many times a URL is accessed """
+    @wraps(func)
     def wrapper(url):
-        cached_key = "cached:" + url
+        count_key = f"count:{url}"
+        store.incr(count_key)
+        return func(url)
+    return wrapper
+
+
+def cache_result(func):
+    """ Decorator caching the result with an expiration time of 10 seconds """
+    @wraps(func)
+    def wrapper(url):
+        cached_key = f"cached:{url}"
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        count_key = "count:" + url
-        html = method(url)
-
-        store.set(cached_key, html)
+        result = func(url)
+        store.set(cached_key, result)
         store.expire(cached_key, 10)
-        return html
+        return result
     return wrapper
 
 
 @count_url_access
+@cache_result
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
+    """ Returns HTML content of a URL """
     res = requests.get(url)
     return res.text
 
